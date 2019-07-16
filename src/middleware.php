b944a018284a99b5d916f49e27d91de9f151c7b2
tdascoli <thomas@dasco.li>
2019-07-16 21:10:18 +0200

    cors

--- src/middleware.php
+++ src/middleware.php
@@ -4,7 +4,9 @@ use Slim\App;
 
 return function (App $app) {
     // e.g: $app->add(new \Slim\Csrf\Guard);
-    $app->add(new \Tuupola\Middleware\JwtAuthentication([
+
+
+    $app->add(new Tuupola\Middleware\JwtAuthentication([
       "path" => "/api",
       "attribute" => "decoded_token_data",
       "secret" => getenv("JWT_SECRET"),
